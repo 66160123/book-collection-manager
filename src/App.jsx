5b4c453c795ea7279ba2c0a476d5c03f3b4063d6
Nutchaya <66160123@go.buu.ac.th>
2025-03-26 15:26:35 +0700

    finish

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ListBooks from "./pages/ListBooks";
+import BookDetail from "./pages/BookDetail";
+import AddEditBook from "./pages/AddEditBook";
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/books" element={<ListBooks />} />
+            <Route path="/books/:id" element={<BookDetail />} />
+            <Route path="/books/add" element={<AddEditBook />} />
+            <Route path="/books/edit/:id" element={<AddEditBook isEditing />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
